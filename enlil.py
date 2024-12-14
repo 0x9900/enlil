7fd1e76b95f3569bfdc5ce5ab7f4a7a188705fe9
Fred C <github-fred@hidzz.com>
2024-12-14 14:33:41 -0800

    Improve the video generation. Change the video size. Better handling of the working directory

--- enlil.py
+++ enlil.py
@@ -13,7 +13,10 @@ import os
 import shutil
 import sys
 import time
+from dataclasses import dataclass
+from pathlib import Path
 from subprocess import PIPE, Popen
+from typing import Any, Dict, Iterator, List, Optional, Type
 from urllib.request import urlretrieve
 
 import yaml
@@ -30,16 +33,51 @@ logging.basicConfig(format='%(asctime)s %(name)s:%(lineno)d %(levelname)s - %(me
 logger = logging.getLogger('enlil')
 
 
-def read_config():
-  home = os.path.expanduser('~')
+class Workdir:
+  def __init__(self, source: Path) -> None:
+    self.workdir = source.joinpath('_workdir')
+
+  def __enter__(self) -> Path:
+    try:
+      self.workdir.mkdir()
+      return self.workdir
+    except IOError as err:
+      raise err
+
+  def __exit__(self, exc_type: Optional[Type[BaseException]],
+               exc_value: Optional[BaseException],
+               traceback: Optional[Type[BaseException]]) -> None:
+    shutil.rmtree(self.workdir)
+
+
+@dataclass(slots=True)
+class Config:
+  target_dir: Path
+  enlil_file: Path
+  video_file: Path
+
+  def __init__(self, *args: Any, **kwargs: Dict[str, str]) -> None:
+    for key, val in kwargs.items():
+      if key not in self.__dataclass_fields__:
+        logging.warning('Configuration attribute: %s ignored', key)
+        continue
+      if isinstance(val, str):
+        tmp_val = Path(val)
+      else:
+        raise TypeError(f"Unexpected type for {key}: {type(val)}")
+      setattr(self, key, tmp_val)
+
+
+def read_config() -> Config:
+  home = Path('~').expanduser()
   config_path = (
-    os.path.join('.', CONFIG_NAME),
-    os.path.join(home, '.' + CONFIG_NAME),
-    os.path.join(home, '.local', CONFIG_NAME),
-    os.path.join('/etc', CONFIG_NAME),
+    Path('.').joinpath(CONFIG_NAME),
+    Path(home).joinpath('.' + CONFIG_NAME),
+    Path(home).joinpath('.local', CONFIG_NAME),
+    Path('/etc').joinpath(CONFIG_NAME),
   )
   for filename in config_path:
-    if os.path.exists(filename):
+    if filename.exists():
       break
     logger.debug('Config file "%s" not found', filename)
   else:
@@ -47,12 +85,17 @@ def read_config():
     sys.exit(os.EX_CONFIG)
 
   logger.debug('Reading config file "%s"', filename)
-  with open(filename, 'r', encoding='utf-8') as confd:
+  with filename.open('r', encoding='utf-8') as confd:
     config = yaml.safe_load(confd)
-  return type('Config', (object,), config)
+
+  try:
+    return Config(**config)
+  except TypeError as err:
+    logger.error('Configuraion error: %s', err)
+    raise SystemExit(err) from None
 
 
-def add_margin(im_name, top, right, bottom, left):
+def add_margin(im_name: Path, top: int, right: int, bottom: int, left: int) -> None:
   color = MARGIN_COLOR
   image = Image.open(im_name)
   width, height = image.size
@@ -60,10 +103,10 @@ def add_margin(im_name, top, right, bottom, left):
   new_height = height + top + bottom
   new_image = Image.new(image.mode, (new_width, new_height), color)
   new_image.paste(image, (left, top))
-  new_image.save(im_name, quality=95)
+  new_image.save(im_name)
 
 
-def retrieve_files(enlil_file, target_dir):
+def retrieve_files(enlil_file: Path, target_dir: Path) -> None:
   try:
     file_time = os.stat(enlil_file).st_mtime
     if time.time() - file_time > 3600:
@@ -74,21 +117,17 @@ def retrieve_files(enlil_file, target_dir):
 
   with open(enlil_file, 'r', encoding='utf-8') as fdin:
     data_source = json.load(fdin)
-    new_cnt = 0
     for url in data_source:
       filename = os.path.basename(url['url'])
-      target_name = os.path.join(target_dir, filename)
+      target_name = target_dir.joinpath(filename)
       if os.path.exists(target_name):
         continue
       urlretrieve(NOAA + url['url'], target_name)
       add_margin(target_name, 0, 0, 50, 0)
       logger.info('%s saved', target_name)
-      new_cnt += 1
-  logger.info('%d new files downloaded', new_cnt)
-  return new_cnt
 
 
-def purge(enlil_file, target_dir):
+def purge(enlil_file: Path, target_dir: Path) -> None:
   """Cleanup old enlil image that are not present in the json manifest"""
   logger.info('Cleaning up non active Enlil images')
   current_files = set([])
@@ -110,37 +149,44 @@ def purge(enlil_file, target_dir):
   logger.info('%d files deleted', count)
 
 
-def select_files(source_dir):
+def counter(start: int = 1) -> Iterator[str]:
+  cnt = start
+  while True:
+    yield f'{cnt:06d}'
+    cnt += 1
+
+
+def select_files(source_dir: Path) -> List[Path]:
   file_list = []
-  for name in os.listdir(source_dir):
-    if not name.startswith('enlil_com') and not name.endswith('.jpg'):
-      continue
+  for name in source_dir.glob('*.jpg'):
     file_list.append(name)
   logger.info('%d files selected for animation', len(file_list))
   return sorted(file_list)
 
 
-def create_links(source_dir, target_dir, file_list):
-  logger.info('Creating workspace %s', target_dir)
-  if not file_list:
-    return
-  for idx, name in enumerate(file_list):
-    target = os.path.join(target_dir, f"enlil-{idx:05d}.jpg")
-    source = os.path.join(source_dir, name)
-    os.link(source, target)
-    logger.debug('Target file: %s', target)
+def create_links(source_dir: Path, workdir: Path, file_list: List[Path]):
+  cnt = counter()
+  for filename in file_list:
+    target = workdir.joinpath(f"enlil-{next(cnt)}.jpg")
+    target.hardlink_to(filename)
+    logger.info('Target file: %s', target)
 
 
-def mk_video(src, video_file):
+def mk_video(work_dir: Path, video_file: Path):
   ffmpeg = shutil.which('ffmpeg')
-  logfile = '/tmp/enlil_animation.log'
-  tmp_file = f"{video_file}-{os.getpid()}.mp4"
-  input_files = os.path.join(src, 'enlil-%05d.jpg')
+  if not ffmpeg:
+    logging.error('"ffmpeg" not found. Make sure it is correctly installed')
+    return
+  logfile = Path('/tmp/enlil_animation.log')
+  tmp_file = work_dir.joinpath(f"{video_file}-{os.getpid()}.mp4")
+  input_files = work_dir.joinpath('enlil-%06d.jpg')
   in_args = f'-y -framerate 10 -i {input_files}'.split()
   ou_args = '-an -c:v libx264 -pix_fmt yuv420p -vf scale=800:542'.split()
-  cmd = [ffmpeg, *in_args, *ou_args, tmp_file]
+  cmd = [ffmpeg, *in_args, *ou_args, str(tmp_file)]
+
   logger.info('Writing ffmpeg output in %s', logfile)
   logger.info("Saving %s video file", tmp_file)
+
   with open(logfile, "a", encoding='ascii') as err:
     err.write(' '.join(cmd))
     err.write('\n\n')
@@ -151,49 +197,23 @@ def mk_video(src, video_file):
       logger.error('Error generating the video file')
       return
     logger.info('mv %s %s', tmp_file, video_file)
-    os.rename(tmp_file, video_file)
+    tmp_file.rename(video_file)
 
 
-def cleanup(directory):
-  for name in os.listdir(directory):
-    os.unlink(os.path.join(directory, name))
-  os.rmdir(directory)
-  logger.info('Working directory "%s" removed', directory)
-
-
-def animate(source_dir, video_file):
-  pid = os.getpid()
-  try:
-    work_dir = os.path.join(source_dir, f"workdir-{pid}")
-    os.mkdir(work_dir)
+def animate(source_dir: Path, video_file: Path):
+  with Workdir(source_dir) as work_dir:
     files = select_files(source_dir)
     create_links(source_dir, work_dir, files)
     mk_video(work_dir, video_file)
-  except KeyboardInterrupt:
-    logger.warning("^C pressed")
-    sys.exit(os.EX_SOFTWARE)
-  finally:
-    cleanup(work_dir)
 
 
 def main():
   logger.setLevel(logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO')))
   config = read_config()
 
-  if not os.path.isdir(config.target_dir):
-    logger.warning("The target directory %s does not exist", config.target_dir)
-    try:
-      os.makedirs(config.target_dir)
-    except IOError as err:
-      logger.error(err)
-      sys.exit(os.EX_IOERR)
-
-  video_file = os.path.join(config.video_dir, config.video_file)
-  if retrieve_files(config.enlil_file, config.target_dir) or not os.path.exists(video_file):
-    purge(config.enlil_file, config.target_dir)
-    animate(config.target_dir, video_file)
-  else:
-    logger.info('No new data')
+  retrieve_files(config.enlil_file, config.target_dir)
+  purge(config.enlil_file, config.target_dir)
+  animate(config.target_dir, config.video_file)
 
 
 if __name__ == "__main__":
